2fdcef60985a24af61ebefc741e48109c55fbca8
Ilya-Detkov <66683957+Ilya-Detkov@users.noreply.github.com>
2020-09-28 11:43:55 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 10000000,
+        speed: 1000
+    });
+});
